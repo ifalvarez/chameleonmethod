f2efe42f4b1505b870ace841454cef09cfd640a4
camilohoyos16 <acamilohoyos2@hotmail.com>
2017-09-11 14:20:36 -0500

    Pause scene

---
+++ VrStealth/Assets/Game/Script/Interface/ButtonsControll.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonsControll : MonoBehaviour 
+{
+
+    private static ButtonsControll instance;
+    private string actualSceneName;
+
+    void Awake()
+    {        
+        //if (instance == null)
+        //    instance = this;
+        //else
+        //    DestroyImmediate(gameObject);
+
+        //DontDestroyOnLoad(this.gameObject);
+    }
+
+	public void LoadScene(string name )
+	{
+        if(name == "MainMenu" )
+            SceneManager.LoadScene(name);
+        else
+        {
+            SceneManager.LoadScene("InterfaceIngame");
+            SceneManager.LoadScene(name, LoadSceneMode.Additive);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void RetryScene()
+    {
+        actualSceneName = GetActualSceneName();
+        
+        SceneManager.UnloadSceneAsync(actualSceneName);
+        SceneManager.LoadSceneAsync(actualSceneName, LoadSceneMode.Additive);
+    }
+
+    public void NextLevel(string nextLevelName)
+    {
+        //string nextLevelName=""; //tomar referencia del nivel
+        SceneManager.UnloadSceneAsync(GetActualSceneName());
+        SceneManager.LoadSceneAsync(nextLevelName, LoadSceneMode.Additive);
+    }
+
+    private string GetActualSceneName()
+	{
+		string name;
+		name = SceneManager.GetSceneAt(1).name;
+		return name;
+	}
+
+    private GameObject canvasToShow;
+    private GameObject canvasToHide;
+
+    public void CanvasToHide(GameObject canvasToHide)
+    {
+       this.canvasToHide = canvasToHide;
+    }
+
+    public void CanvasToShow(GameObject canvasToShow)
+    {
+        this.canvasToShow = canvasToShow;
+    }
+
+    public void ChangeCanvas()
+    {
+        canvasToHide.SetActive(false);
+        canvasToShow.SetActive(true);
+    }
+
+    private bool isPuase;
+
+    public void Pause(GameObject pauseCanvas)
+    {
+        pauseCanvas.SetActive(!pauseCanvas.activeInHierarchy);
+    }
+}
---
+++ VrStealth/Assets/Game/Script/Interface/PauseBug.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseBug : MonoBehaviour {
+
+    [SerializeField] private string tongeTag;
+    [SerializeField] private GameObject pauseCanvas;
+    [SerializeField] private GameObject player;
+
+    private BoxCollider coll;
+    private MeshRenderer mesh;
+    private bool canvasIsActive;
+    private bool isInteractable;
+    private Vector3 bugPosition;
+
+    private void Start()
+    {
+        coll = GetComponent<BoxCollider>();
+        mesh = GetComponent<MeshRenderer>();
+        player = GameObject.Find("Player");
+        bugPosition = new Vector3(player.transform.position.x + 5, player.transform.position.y + 5, player.transform.position.z + 5);
+    }
+
+
+    void OnTriggerEnter(Collider other)
+    {
+        Pause();
+        if(other.transform.tag == tongeTag)
+        {
+            
+        }
+    }
+
+    void Update()
+    {
+        transform.position = bugPosition;
+        if(!isInteractable)
+        {
+            if(canvasIsActive == pauseCanvas.activeInHierarchy)
+            {
+                PauseOff();
+                isInteractable = true;
+
+            }
+        }
+    }
+
+ /*   void OnMouseDown()
+    {
+        Pause();
+    }*/
+
+    private void PauseOff()
+    {
+        coll.enabled = true;
+        mesh.enabled = true;
+    }
+
+    public void Pause()
+    {
+        coll.enabled = false;
+        mesh.enabled = false;
+        pauseCanvas.transform.position = transform.position;
+        pauseCanvas.transform.forward = (player.transform.position - transform.position).normalized * -1;
+        isInteractable = false;
+        canvasIsActive = pauseCanvas.activeInHierarchy;
+        pauseCanvas.SetActive(!pauseCanvas.activeInHierarchy);
+    }
+
+}
